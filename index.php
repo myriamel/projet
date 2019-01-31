f824285dbd1716c250e97c44b82e72d901b7e579
myriamel <galaxy.teos.50@gmail.com>
2019-01-31 13:51:42 +0100

    modif loal 13h51

--- index.php
+++ index.php
@@ -4,4 +4,8 @@
 echo "Hello World";
 // commentaire en ligne
 // commentaire atom
+echo "je modifie en local sur la branche windows";
+// creer un conflit a partir du local
+// commentaire branche enligne
+// modif github 13h44
  ?>
